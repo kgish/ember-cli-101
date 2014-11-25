6029889750bdd616da82bf517786782a73e64f19
Kiffin Gish <kiffin.gish@planet.nl>
2014-11-25 15:11:09 +0100

    Lending new articles.

--- app/models/article.js
+++ app/models/article.js
@@ -3,7 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   description: DS.attr('string'),
   createdAt: DS.attr('date'),
-  state: DS.attr('string'),
+  state: DS.attr('string', {
+    defaultValue: 'borrowed'
+  }),
   notes: DS.attr('string'),
   friend: DS.belongsTo('friend')
 });
